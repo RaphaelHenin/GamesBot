4bc1d2ef4bf8a765ecf2bf356eeefaacc628878e
Raphael Henin <heniin.raphael@gmail.com>
2019-03-31 18:08:36 +0200

    Echo what the user say

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def receive_message():
                 recipient_id = message['sender']['id']
                 if message['message'].get('text'):
                     #response_sent_text = get_message()                 
-                    send_message(recipient_id, bot_response(message['message'].get('text')))
+                    send_message(recipient_id,message['message'].get('text'))
                 #if user sends us a GIF, photo,video, or any other non-text item
                 if message['message'].get('attachments'):
                     response_sent_nontext = get_message()
